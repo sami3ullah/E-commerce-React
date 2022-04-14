dbcd3786dc0615f31e8cda5c8e975f739ab43cf0
Muhammad Samiullah <msami398@gmail.com>
2022-04-14 21:24:20 +0500

    created user reducer and root reducer(small scale)

---
+++ src/redux/root-reducer.js
@@ -0,0 +1,9 @@
+// this file is the one big object of all the reducers that will be used in the store
+
+import { combineReducers } from "redux";
+import userReducer from "./user/user.reducer";
+
+// for combining all the reducers that we have in our app
+export default combineReducers({
+  user: userReducer,
+});
