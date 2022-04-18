18e40bdb7dd9f22bad0acb6924e94d8dc4110b5d
Muhammad Samiullah <msami398@gmail.com>
2022-04-19 03:10:11 +0500

    added toggle functionality to the cart icon with redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,7 +8,7 @@ import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 // css
 import "./header.styles.scss";
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
@@ -32,13 +32,14 @@ const Header = ({ currentUser }) => {
         )}
         <CartIcon />
       </div>
-      <CartDropdown />
+      {hidden ? null : <CartDropdown />}
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,8 +2,10 @@
 
 import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
+import cartReducer from "./cart/cart.reducer";
 
 // for combining all the reducers that we have in our app
 export default combineReducers({
   user: userReducer,
+  cart: cartReducer,
 });
